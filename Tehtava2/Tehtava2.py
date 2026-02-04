d1f4381d7360858d7bbea618cf71dbcdcea8bdcf
saukk1 <saulusr@metropolia.fi>
2026-02-04 15:19:24 +0200

    lessgo

---
+++ Tehtava2/Tehtava2.py
@@ -0,0 +1,16 @@
+name=input("Mikä on nimesi:")
+
+print("Nice to meet you"+ name)
+print("Nice to meet you",name)
+print(f"Hello {name}!")
+
+age = 18
+
+print(age)
+
+
+
+merkkijono = string
+kokonaisluku= integer desimaali =float
+boolean= totuusarvo true tai false
+yhtee
---
+++ Tehtava2/testi.py
@@ -0,0 +1,40 @@
+name = "Saulus"
+lisa = 10
+age= int(input("Kuinka vanha olet?"))
+print(f"Olet {age + lisa} vuotta vanha.")
+
+numero1 =100
+numero2 ="100"
+print(numero1+numero1)
+print(numero2+numero2)
+print(numero1, numero2)
+
+luku1 = float(input("Anna eka luku"))
+luku2 = float(input("Anna toka luku"))
+
+print(luku1+luku2)
+print(luku1-luku2)
+print(luku1/luku2)
+print(luku1*luku2)
+print(luku1//luku2)
+print(luku1**luku2)
+print(luku1%luku2)
+print(luku1>luku2)
+print(luku1<luku2)
+
+pituus = int(input( "Anna pituutesi:"))
+paino = float(input("Anna painosi:"))
+
+
+
+bmi = paino / (pituus/100) **2
+print("BMI:si on", bmi)
+
+import math
+print(math.pi)
+print(math.e)
+print(math.log)
+print(math.sin)
+print(math.tan)
+print(math.cos)
+
